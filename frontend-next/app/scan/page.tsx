241c729c496e3abdbeaf981591fd3dc23253c267
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-09-06 19:57:31 +0200

    added working qr scan component

--- frontend-next/app/scan/page.tsx
+++ frontend-next/app/scan/page.tsx
@@ -1,15 +1,43 @@
 "use client";
 
-import { title } from "@/components/primitives";
-import { Scanner } from "@yudiel/react-qr-scanner";
+import {title} from "@/components/primitives";
+import {QrReader} from "react-qr-reader";
+import {useState} from "react";
 
 export default function ScanPage() {
-  return (
-    <div>
-      <h1 className={title()}>Scan</h1>
-      <div style={{ width: "400px" }}>
-        <Scanner onScan={(result) => console.log(result)} />
-      </div>
-    </div>
-  );
+    const [data, setData] = useState("No result");
+    const [error, setError] = useState(null);
+
+    const handleScan = (result: any) => {
+        if (result) {
+            setData(result.text);
+        }
+    };
+
+    const handleError = (err: any) => {
+        setError(err.message);
+    };
+
+    return (
+        <div>
+            <h1 className={title()}>Scan</h1>
+            <div>
+                <h2>QR Code Scanner</h2>
+                <QrReader
+                    onResult={(result: any, error: any) => {
+                        if (result) {
+                            handleScan(result);
+                        }
+
+                        if (error) {
+                            handleError(error);
+                        }
+                    }}
+                    constraints={{facingMode: "environment"}}
+                />
+                <p>Scanned Data: {data}</p>
+                {error && <p style={{color: "red"}}>Error: {error}</p>}
+            </div>
+        </div>
+    );
 }
--- frontend-next/components/navbar.tsx
+++ frontend-next/components/navbar.tsx
@@ -88,7 +88,7 @@ export const Navbar = () => {
                       ? "danger"
                       : "foreground"
                 }
-                href="#"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
