1552fec795d2e4f8ebbddd15e07e6a8febe111ba
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-09-12 17:33:52 +0200

    updated navbar to always show links

--- frontend-next/components/navbar.tsx
+++ frontend-next/components/navbar.tsx
@@ -15,88 +15,89 @@ import clsx from "clsx";
 
 import {siteConfig} from "@/config/site";
 import {ThemeSwitch} from "@/components/theme-switch";
-import {GithubIcon, Logo,} from "@/components/icons";
+import {GithubIcon, Logo} from "@/components/icons";
 
 export const Navbar = () => {
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">LARPlaner</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+      <NextUINavbar maxWidth="xl" position="sticky">
+        {/* Left-aligned brand */}
+        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+          <NavbarBrand as="li" className="gap-3 max-w-fit">
+            <NextLink className="flex justify-start items-center gap-1" href="/">
+              <Logo />
+              <p className="font-bold text-inherit">LARPlaner</p>
+            </NextLink>
+          </NavbarBrand>
+          {/* Desktop links */}
+          <ul className="hidden sm:flex gap-4 justify-start ml-2">
+            {siteConfig.navItems.map((item) => (
+                <NavbarItem key={item.href}>
+                  <NextLink
+                      className={clsx(
+                          linkStyles({color: "foreground"}),
+                          "data-[active=true]:text-primary data-[active=true]:font-medium"
+                      )}
+                      color="foreground"
+                      href={item.href}
+                  >
+                    {item.label}
+                  </NextLink>
+                </NavbarItem>
+            ))}
+          </ul>
+        </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
+        {/* Right-aligned content */}
+        <NavbarContent className="basis-1/5 flex  sm:basis-full" justify="end">
+          <NavbarItem className="hidden sm:flex gap-2">
+            <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+              <GithubIcon className="text-default-500" />
+            </Link>
+            <ThemeSwitch />
+          </NavbarItem>
+          <NavbarItem className="hidden sm:flex">
+            <Button
+                as={Link}
+                className="text-sm font-normal text-default-600 bg-default-100"
+                href={"/login"}
+                variant="flat"
+            >
+              Log in
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+
+        {/* Mobile menu toggle */}
+        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
-        </NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={"/login"}
-            variant="flat"
-          >
-            Log in
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+          <NavbarMenuToggle />
+        </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
-
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href={item.href}
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
+        {/* Mobile menu items */}
+        <NavbarMenu className="sm:hidden">
+          <div className="mx-4 mt-2 flex flex-col gap-2">
+            {siteConfig.navMenuItems.map((item, index) => (
+                <NavbarMenuItem key={`${item}-${index}`}>
+                  <Link
+                      color={
+                        index === 2
+                            ? "primary"
+                            : index === siteConfig.navMenuItems.length - 1
+                                ? "danger"
+                                : "foreground"
+                      }
+                      href={item.href}
+                      size="lg"
+                  >
+                    {item.label}
+                  </Link>
+                </NavbarMenuItem>
+            ))}
+          </div>
+        </NavbarMenu>
+      </NextUINavbar>
   );
 };
