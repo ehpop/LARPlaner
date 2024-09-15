c7877ceecde0606e1dc9012013ffc277a51c3539
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2024-09-15 15:33:19 +0200

    updated paths to have separate views for admin and regular users

--- frontend-next/components/navbar.tsx
+++ frontend-next/components/navbar.tsx
@@ -1,11 +1,13 @@
+"use client";
+
 import {
-  Navbar as NextUINavbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
+    Navbar as NextUINavbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
 } from "@nextui-org/navbar";
 import {Button} from "@nextui-org/button";
 import {Link} from "@nextui-org/link";
@@ -16,88 +18,87 @@ import clsx from "clsx";
 import {siteConfig} from "@/config/site";
 import {ThemeSwitch} from "@/components/theme-switch";
 import {GithubIcon, Logo} from "@/components/icons";
+import {useState} from "react";
 
 export const Navbar = () => {
-  return (
-      <NextUINavbar maxWidth="xl" position="sticky">
-        {/* Left-aligned brand */}
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-          <NavbarBrand as="li" className="gap-3 max-w-fit">
-            <NextLink className="flex justify-start items-center gap-1" href="/">
-              <Logo />
-              <p className="font-bold text-inherit">LARPlaner</p>
-            </NextLink>
-          </NavbarBrand>
-          {/* Desktop links */}
-          <ul className="hidden sm:flex gap-4 justify-start ml-2">
-            {siteConfig.navItems.map((item) => (
-                <NavbarItem key={item.href}>
-                  <NextLink
-                      className={clsx(
-                          linkStyles({color: "foreground"}),
-                          "data-[active=true]:text-primary data-[active=true]:font-medium"
-                      )}
-                      color="foreground"
-                      href={item.href}
-                  >
-                    {item.label}
-                  </NextLink>
+    const [isAdmin, setIsAdmin] = useState(true);
+    const [navItems, setNavItems] = useState(isAdmin ?  siteConfig.navItems.admin :  siteConfig.navItems.user);
+    const [navMenuItems, setNavMenuItems] = useState(isAdmin ? siteConfig.navMenuItems.admin : siteConfig.navMenuItems.user);
+
+    return (
+        <NextUINavbar maxWidth="xl" position="sticky">
+            {/* Left-aligned brand */}
+            <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+                <NavbarBrand as="li" className="gap-3 max-w-fit">
+                    <NextLink className="flex justify-start items-center gap-1" href="/">
+                        <Logo/>
+                        <p className="font-bold text-inherit">LARPlaner</p>
+                    </NextLink>
+                </NavbarBrand>
+                {/* Desktop links */}
+                <ul className="hidden sm:flex gap-4 justify-start ml-2">
+                    {navItems.map((item) => (
+                        <NavbarItem key={item.href}>
+                            <NextLink
+                                className={clsx(
+                                    linkStyles({color: "foreground"}),
+                                    "data-[active=true]:text-primary data-[active=true]:font-medium"
+                                )}
+                                color="foreground"
+                                href={item.href}
+                            >
+                                {item.label}
+                            </NextLink>
+                        </NavbarItem>
+                    ))}
+                </ul>
+            </NavbarContent>
+
+            {/* Right-aligned content */}
+            <NavbarContent className="basis-1/5 flex  sm:basis-full" justify="end">
+                <NavbarItem className="hidden sm:flex gap-2">
+                    <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+                        <GithubIcon className="text-default-500"/>
+                    </Link>
+                    <ThemeSwitch/>
                 </NavbarItem>
-            ))}
-          </ul>
-        </NavbarContent>
+                <NavbarItem className="hidden sm:flex">
+                    <Button
+                        as={Link}
+                        className="text-sm font-normal text-default-600 bg-default-100"
+                        href={"/login"}
+                        variant="flat"
+                    >
+                        Log in
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
 
-        {/* Right-aligned content */}
-        <NavbarContent className="basis-1/5 flex  sm:basis-full" justify="end">
-          <NavbarItem className="hidden sm:flex gap-2">
-            <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-              <GithubIcon className="text-default-500" />
-            </Link>
-            <ThemeSwitch />
-          </NavbarItem>
-          <NavbarItem className="hidden sm:flex">
-            <Button
-                as={Link}
-                className="text-sm font-normal text-default-600 bg-default-100"
-                href={"/login"}
-                variant="flat"
-            >
-              Log in
-            </Button>
-          </NavbarItem>
-        </NavbarContent>
+            {/* Mobile menu toggle */}
+            <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+                <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+                    <GithubIcon className="text-default-500"/>
+                </Link>
+                <ThemeSwitch/>
+                <NavbarMenuToggle/>
+            </NavbarContent>
 
-        {/* Mobile menu toggle */}
-        <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
-          </Link>
-          <ThemeSwitch />
-          <NavbarMenuToggle />
-        </NavbarContent>
-
-        {/* Mobile menu items */}
-        <NavbarMenu className="sm:hidden">
-          <div className="mx-4 mt-2 flex flex-col gap-2">
-            {siteConfig.navMenuItems.map((item, index) => (
-                <NavbarMenuItem key={`${item}-${index}`}>
-                  <Link
-                      color={
-                        index === 2
-                            ? "primary"
-                            : index === siteConfig.navMenuItems.length - 1
-                                ? "danger"
-                                : "foreground"
-                      }
-                      href={item.href}
-                      size="lg"
-                  >
-                    {item.label}
-                  </Link>
-                </NavbarMenuItem>
-            ))}
-          </div>
-        </NavbarMenu>
-      </NextUINavbar>
-  );
+            {/* Mobile menu items */}
+            <NavbarMenu className="sm:hidden">
+                <div className="mx-4 mt-2 flex flex-col gap-2">
+                    {navMenuItems.map((item, index) => (
+                        <NavbarMenuItem key={`${item}-${index}`}>
+                            <Link
+                                color="primary"
+                                href={item.href}
+                                size="lg"
+                            >
+                                {item.label}
+                            </Link>
+                        </NavbarMenuItem>
+                    ))}
+                </div>
+            </NavbarMenu>
+        </NextUINavbar>
+    );
 };
--- frontend-next/config/site.ts
+++ frontend-next/config/site.ts
@@ -3,50 +3,94 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
     name: "LARPlaner",
     description: "LARPlaner - plan and conduct LARP events.",
-    navItems: [
-        {
-            label: "Home",
-            href: "/",
-        },
-        {
-            label: "Events",
-            href: "/events",
-        },
-        {
-            label: "Scenarios",
-            href: "/scenarios",
-        },
-        {
-            label: "Roles",
-            href: "/roles",
-        },
-        {
-            label: "Scan",
-            href: "/scan",
-        },
-    ],
-    navMenuItems: [
-        {
-            label: "Home",
-            href: "/",
-        },
-        {
-            label: "Events",
-            href: "/events",
-        },
-        {
-            label: "Scenarios",
-            href: "/scenarios",
-        },
-        {
-            label: "Roles",
-            href: "/roles",
-        },
-        {
-            label: "Scan",
-            href: "/scan",
-        },
-    ],
+    navItems: {
+        admin: [
+            {
+                label: "Home",
+                href: "/",
+            },
+            {
+                label: "Events",
+                href: "/admin/events",
+            },
+            {
+                label: "Scenarios",
+                href: "/admin/scenarios",
+            },
+            {
+                label: "Roles",
+                href: "/admin/roles",
+            },
+            {
+                label: "Scan",
+                href: "/admin/scan",
+            },
+        ],
+        user: [
+            {
+                label: "Home",
+                href: "/",
+            },
+            {
+                label: "Events",
+                href: "/admin/events",
+            },
+            {
+                label: "Scenarios",
+                href: "/admin/scenarios",
+            },
+            {
+                label: "Roles",
+                href: "/admin/roles",
+            },
+            {
+                label: "Roles",
+                href: "/admin/scan",
+            },
+        ],
+    },
+    navMenuItems: {
+        admin: [
+            {
+                label: "Home",
+                href: "/",
+            },
+            {
+                label: "Events",
+                href: "/events",
+            },
+            {
+                label: "Scenarios",
+                href: "/scenarios",
+            },
+            {
+                label: "Roles",
+                href: "/roles",
+            },
+            {
+                label: "Scan",
+                href: "/scan",
+            },
+        ],
+        user: [
+            {
+                label: "Home",
+                href: "/",
+            },
+            {
+                label: "Events",
+                href: "/events",
+            },
+            {
+                label: "Scenarios",
+                href: "/scenarios",
+            },
+            {
+                label: "Roles",
+                href: "/roles",
+            },
+        ],
+    },
     links: {
         github: "https://github.com/ehpop/LARPlaner",
     },
