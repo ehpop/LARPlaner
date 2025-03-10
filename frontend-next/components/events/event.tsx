f5791abd3580876243b83aaffc2fabf802d44eee
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2025-03-10 19:13:16 +0100

    small fixes in event.tsx, changed hook file extension from .tsx to .ts

--- frontend-next/components/events/event.tsx
+++ frontend-next/components/events/event.tsx
@@ -9,18 +9,21 @@ interface EventProps {
 
 export const Event = ({ event }: EventProps) => {
   const card = (
-    <div className="w-min flex flex-row">
+    <div className="w-full max-w-sm mx-auto flex flex-row">
       <Card key={event.id} className="mx-3" shadow="sm">
-        <CardBody className="p-0 h-[240px] w-[390px]">
-          <Image
-            alt={event.name}
-            className="object-cover h-[240px] w-[400px]"
-            fallbackSrc="/images/event-fallback.png"
-            radius="lg"
-            shadow="sm"
-            src={event.img}
-          />
-        </CardBody>
+        {/* Ensure proportional resizing */}
+        <div className="relative w-full aspect-w-16 aspect-h-9">
+          <CardBody className="p-0 h-full">
+            <Image
+              alt={event.name}
+              className="object-cover h-full w-full"
+              fallbackSrc="/images/event-fallback.png"
+              radius="lg"
+              shadow="sm"
+              src={event.img}
+            />
+          </CardBody>
+        </div>
         <CardFooter className="text-small justify-between">
           <b>{event.name}</b>
           <p className="text-default-500">
