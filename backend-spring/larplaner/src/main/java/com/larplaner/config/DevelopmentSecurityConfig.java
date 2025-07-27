ad54442807625cb1d7c4d2b002f91528b0d8c552
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2025-07-28 01:34:43 +0200

    updated security config for prod and dev

--- backend-spring/larplaner/src/main/java/com/larplaner/config/DevelopmentSecurityConfig.java
+++ backend-spring/larplaner/src/main/java/com/larplaner/config/DevelopmentSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.larplaner.config;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import com.larplaner.security.FirebaseTokenFilter;
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +13,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -22,8 +25,8 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 @EnableMethodSecurity
 @Slf4j
-@Profile({"prod"})
-public class SecurityConfig {
+@Profile({"dev", "dev-neondb"})
+public class DevelopmentSecurityConfig {
 
   @Bean
   public FirebaseTokenFilter firebaseTokenFilter() {
@@ -38,19 +41,21 @@ public class SecurityConfig {
         // Configure CORS
         .cors(customizer -> customizer.configurationSource(corsConfigurationSource()))
         // Make session management stateless
-        .sessionManagement(
-            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         // Apply authorization rules
         .authorizeHttpRequests(auth -> auth
             // --- Public endpoints ---
+            .requestMatchers(toH2Console()).permitAll()
             .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
 
             // --- API Authorization Rules ---
-            .requestMatchers("/api/**").authenticated()
+            .requestMatchers( "/api/**").authenticated()
 
             // Fallback: any other request must be authenticated
             .anyRequest().authenticated()
         )
+        // Allow frames for H2 console
+        .headers(headers -> headers.frameOptions(FrameOptionsConfig::sameOrigin))
         .addFilterBefore(firebaseTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
@@ -60,21 +65,18 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
 
-    configuration.setAllowedOrigins(List.of("http://localhost:9000, https://larplaner.vercel.app"));
+    configuration.setAllowedOrigins(List.of("http://localhost:9000"));
 
-    configuration.setAllowedMethods(
-        Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
-    configuration.setAllowedHeaders(
-        Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
 
     configuration.setAllowCredentials(true);
 
-    configuration.setExposedHeaders(List.of("Authorization"));
+    // configuration.setExposedHeaders(List.of("Authorization"));
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**",
-        configuration); // Apply this configuration to all endpoints
+    source.registerCorsConfiguration("/**", configuration); // Apply this configuration to all endpoints
 
     return source;
   }
