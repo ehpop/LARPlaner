d8436efff5b40de27724e7e722c218ed2ffd880d
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2025-07-28 00:41:16 +0200

    still debugging

--- backend-spring/larplaner/src/main/java/com/larplaner/config/FirebaseConfig.java
+++ backend-spring/larplaner/src/main/java/com/larplaner/config/FirebaseConfig.java
@@ -5,8 +5,6 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.File;
-import java.nio.file.Path;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,26 +17,9 @@ public class FirebaseConfig {
   @Value("${firebase.service-account.path}")
   private Resource serviceAccountResource;
 
-  private void printDirectoryContents(File directory) {
-    System.out.println("Contents of directory: " + directory.getAbsolutePath());
-    File[] files = directory.listFiles();
-    if (files != null) {
-      for (File file : files) {
-        System.out.println(file.getName());
-      }
-    }
-  }
-
   @PostConstruct
   public void initializeFirebase() throws IOException {
-    try {
-      File currentDir = new File(".");
-      printDirectoryContents(currentDir);
-      printDirectoryContents(currentDir.getParentFile());
-      printDirectoryContents(currentDir.getParentFile().getParentFile());
-
-      InputStream serviceAccountStream = serviceAccountResource.getInputStream();
-
+    try (InputStream serviceAccountStream = serviceAccountResource.getInputStream()) {
       FirebaseOptions options = FirebaseOptions.builder()
           .setCredentials(GoogleCredentials.fromStream(serviceAccountStream))
           .build();
@@ -49,7 +30,6 @@ public class FirebaseConfig {
       } else {
         System.out.println("Firebase Admin SDK already initialized.");
       }
-
     } catch (IOException e) {
       System.err.println("Error initializing Firebase Admin SDK: " + e.getMessage());
       throw e;
