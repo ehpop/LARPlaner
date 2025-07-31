962a270fe40dedb504bd74a7523192967ed4aa68
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2025-08-01 00:08:35 +0200

    Removed obsolete test files to declutter the codebase.

--- backend-spring/larplaner/src/test/java/com/larplaner/api/common/BaseTestClass.java
+++
@@ -1,8 +0,0 @@
-package com.larplaner.api.common;
-
-import org.springframework.context.annotation.Import;
-
-@Import(com.larplaner.config.TestSecurityConfig.class)
-public abstract class BaseTestClass {
-
-}
