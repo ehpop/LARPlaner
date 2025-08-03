443093da8a52113128b41bd745985eff74f3d5d7
Tomasz Tkaczyk <95701763+ehpop@users.noreply.github.com>
2025-08-03 23:03:37 +0200

    NullPointer fix

--- backend-spring/larplaner/src/main/java/com/larplaner/mapper/game/GameSessionMapper.java
+++ backend-spring/larplaner/src/main/java/com/larplaner/mapper/game/GameSessionMapper.java
@@ -1,8 +1,9 @@
 package com.larplaner.mapper.game;
 
+import static com.larplaner.mapper.common.MapperHelper.getIdSafe;
+
 import com.larplaner.dto.game.GameSessionDetailedResponseDTO;
 import com.larplaner.dto.game.GameSessionSummaryResponseDTO;
-import com.larplaner.mapper.game.action.GameActionLogMapper;
 import com.larplaner.mapper.game.item.GameItemStateMapper;
 import com.larplaner.mapper.game.role.GameRoleStateMapper;
 import com.larplaner.model.game.GameSession;
@@ -16,14 +17,11 @@ public class GameSessionMapper {
 
   private final GameRoleStateMapper gameRoleStateMapper;
   private final GameItemStateMapper gameItemStateMapper;
-  private final GameActionLogMapper gameActionLogMapper;
 
   public GameSessionMapper(@Lazy GameRoleStateMapper gameRoleStateMapper,
-      @Lazy GameItemStateMapper gameItemStateMapper,
-      @Lazy GameActionLogMapper gameActionLogMapper) {
+      @Lazy GameItemStateMapper gameItemStateMapper) {
     this.gameRoleStateMapper = gameRoleStateMapper;
     this.gameItemStateMapper = gameItemStateMapper;
-    this.gameActionLogMapper = gameActionLogMapper;
   }
 
   public GameSessionDetailedResponseDTO toDetailedDTO(GameSession gameSession) {
@@ -33,7 +31,7 @@ public class GameSessionMapper {
 
     return GameSessionDetailedResponseDTO.builder()
         .id(gameSession.getId())
-        .eventId(gameSession.getEvent().getId())
+        .eventId(getIdSafe(gameSession.getEvent()))
         .startTime(gameSession.getStartTime())
         .endTime(gameSession.getEndTime())
         .assignedRoles(gameSession.getAssignedRoles() != null
@@ -58,7 +56,7 @@ public class GameSessionMapper {
         .id(gameSession.getId())
         .startTime(gameSession.getStartTime())
         .endTime(gameSession.getEndTime())
-        .eventId(gameSession.getEvent().getId())
+        .eventId(getIdSafe(gameSession.getEvent()))
         .build();
   }
 
